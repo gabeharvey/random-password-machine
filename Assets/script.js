1a30a715134917be68b0222b1ebb57814619426b
Gabe Harvey <harvey7@gmail.com>
2023-12-09 12:59:30 -0600

    adjustments to functions, variables, and text

--- Assets/script.js
+++ Assets/script.js
@@ -1,11 +1,10 @@
-// Assignment code here
+// Variables used for password generation
 let passRes = [];
 let resUpperCase = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
 let resLowerCase = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
 let resNumber = ["0","1","2","3","4","5","6","7","8","9","10"];
 let resSymbol = ["!","@","#","$","%","^","&","*","(",")","_","+","{","}","|","[","]",":","<",">","?"];
 
-// Get references to the #generate element
 let generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
@@ -21,7 +20,7 @@ function writePassword() {
 
 function generatePassword () {
   let password = "";
-  for(var i = 0; i < passLength; i++) {
+  for(let i = 0; i < passLength; i++) {
     let randomPass = Math.floor(Math.random() * passRes.length);
     password = password + passRes[randomPass];
   }
@@ -32,26 +31,26 @@ function generatePassword () {
 generateBtn.addEventListener("click", writePassword);
 
 function resPrompt() {
-  passLength = parseInt(prompt("Please choose a password length between 8 characters and 128 characters by entering number of characters desired."));
+  passLength = parseInt(prompt("Please choose a password length between 8 characters and 128 characters by entering number of characters desired in the box below 👇."));
   
   if (isNaN(passLength) || passLength > 128 || passLength < 8) {
-    alert("Password length must be between 8 characters and 128 characters. Please enter a number on the keyboard or number keypad.");
+    alert("🚫 Answer Invalid. 🚫 Please choose a number between 8 and 128 on the keypad. ex. 1️⃣2️⃣3️⃣");
     return false;
   }
 
-  if (confirm("Include UPPERCASE letters in password? Please answer YES or NO.")) {
+  if (confirm("Include UPPERCASE letters in password? Click 'OK' for Yes and 'Cancel' for No.")) {
     passRes = passRes.concat(resUpperCase);
   }
 
-  if (confirm("Include lowercase letters in password? Please answer YES or NO")) {
+  if (confirm("Include lowercase letters in password? Click 'OK' for Yes and 'Cancel' for No.")) {
     passRes = passRes.concat(resLowerCase);
   }
 
-  if (confirm("Include numbers in password? Please answer YES or NO")) {
+  if (confirm("Include numbers in password? Click 'OK' for Yes and 'Cancel' for No.")) {
     passRes = passRes.concat(resNumber);
   }
 
-  if (confirm("Inclue symbols in password? Ex. ! @ # $ % Please answer YES or NO")) {
+  if (confirm("Inclue symbols in password? Ex. ! @ # $ % Click 'OK' for Yes and 'Cancel' for No.")) {
     passRes = passRes.concat(resSymbol);
   }
   return true;
