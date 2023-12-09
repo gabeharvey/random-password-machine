2f167ae353705e5c8b630236f54cb604776eafa6
Gabe Harvey <harvey7@gmail.com>
2023-12-09 10:00:29 -0600

    added js variables

--- Assets/script.js
+++ Assets/script.js
@@ -1,5 +1,10 @@
 // Assignment code here
-
+let passLength = 16;
+let passRes = [];
+let resUpperCase = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
+let resLowerCase = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
+let resNumber = ["0","1","2","3","4","5","6","7","8","9","10"];
+let resSymbol = ["!","@","#","$","%","^","&","*","(",")","_","+","{","}","|","[","]",":","<",">","?"];
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
