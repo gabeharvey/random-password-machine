edc72024b16598ffb19955d7977041d29d7ee9c3
Gabe Harvey <harvey7@gmail.com>
2023-12-09 11:38:10 -0600

    added js functions

--- Assets/script.js
+++ Assets/script.js
@@ -1,5 +1,4 @@
 // Assignment code here
-let passLength = 16;
 let passRes = [];
 let resUpperCase = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
 let resLowerCase = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
@@ -7,23 +6,32 @@ let resNumber = ["0","1","2","3","4","5","6","7","8","9","10"];
 let resSymbol = ["!","@","#","$","%","^","&","*","(",")","_","+","{","}","|","[","]",":","<",">","?"];
 
 // Get references to the #generate element
-var generateBtn = document.querySelector("#generate");
+let generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+  let validPass = resPrompt();
+  let passwordText = document.querySelector("#password");
+  
+  if(validPass) {
+  let finalPass = generatePassword();
+  passwordText.value = finalPass;
+  }
+}
 
-  passwordText.value = password;
-
+function generatePassword () {
+  let password = "";
+  for(var i = 0; i < passLength; i++) {
+    let randomPass = Math.floor(Math.random() * passRes.length);
+    password = password + passRes[randomPass];
+  }
+  return password;
 }
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
 function resPrompt() {
-  passRes [];
-  
   passLength = parseInt(prompt("Please choose a password length between 8 characters and 128 characters by entering number of characters desired."));
   
   if (isNaN(passLength) || passLength > 128 || passLength < 8) {
